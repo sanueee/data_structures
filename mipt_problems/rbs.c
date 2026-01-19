274c117a322b390f1ca73d28049007e2e4716243
sanueee <109550742+sanueee@users.noreply.github.com>
2026-01-19 16:52:38 +0300

    solved rbs

--- mipt_problems/rbs.c
+++ mipt_problems/rbs.c
@@ -4,9 +4,8 @@
 
 struct node_t {struct node_t *next; int data;};
 
-struct node_t * read_list(int len);
-void delete_list(struct node_t *top);
-void print_list(const struct node_t *top);
+struct node_t* insert(struct node_t *head, int el);
+void print_and_free(struct node_t **buckets, int len);
 
 int main(void) {
     int len;
@@ -14,50 +13,60 @@ int main(void) {
         perror("wrong value of len");
         return -1;
     }
-    struct node_t *list = read_list(len);
-    if (!list) {
-        perror("error creating list");
-        return -1;
+
+    int max = 0;
+    int *arr = malloc(len * sizeof(int));
+
+    for (int i = 0; i < len; i++) {
+        scanf("%d", &arr[i]);
+        if (arr[i] > max) max = arr[i];
     }
 
-    
+    struct node_t **buckets = calloc(len, sizeof(struct node_t*));
+    int bucket_size = max / len;
+    if (bucket_size == 0) bucket_size = 1;
 
-    print_list(list);
-    delete_list(list);
+    for (int i = 0; i < len; i++) {
+        int bucket_index = arr[i] / bucket_size;
+        if (bucket_index >= len) bucket_index = len - 1;
+        buckets[bucket_index] = insert(buckets[bucket_index], arr[i]); // возвращаем голову bucket'a
+    }
+
+    print_and_free(buckets, len);
 
     return 0;
 }
 
-struct node_t* read_list(int len) {
-    int num;
-    if (scanf("%d", &num) != 1) {
-        return NULL;
+struct node_t* insert(struct node_t *head, int el) {
+    struct node_t *new_node = calloc(1, sizeof(struct node_t));
+    new_node->data = el;
+    new_node->next = NULL;
+
+    if (!head || head->data >= el) { // если вставляем в самое начало (надо изменить голову)
+        new_node->next = head;
+        return new_node;
     }
-    struct node_t *top = calloc(1, sizeof(struct node_t));
-    top->data = num;
-    struct node_t *curr = top;
-    while (scanf("%d", &num) == 1 && --len) {
-        struct node_t *new_node = calloc(1, sizeof(struct node_t));
-        new_node->data = num;
-        curr->next = new_node;
-        curr = new_node;
+
+    struct node_t *curr = head;
+    while (curr->next && curr->next->data <= el) {
+        curr = curr->next;
     }
-    return top;
+    new_node->next = curr->next;
+    curr->next = new_node;
+
+    return head;
 }
 
-void delete_list(struct node_t *top) {
-    struct node_t *current = top;
-    while (current != NULL) {
-        struct node_t *temp = current->next;
-        free(current);
-        current = temp;
-    }
-}
-
-void print_list(const struct node_t *top) {
-    const struct node_t *p;
-    for (p = top; p != NULL; p = p->next) {
-        printf("%d ", p->data);
+void print_and_free(struct node_t **buckets, int len) {
+    for (int i = 0; i < len; i++) {
+        struct node_t *curr = *(buckets + i);
+        while (curr) {
+            printf("%d ", curr->data);
+            struct node_t *temp = curr;
+            curr = curr->next;
+            free(temp); // очищаем по указателю
+        }
+        printf("0 ");
     }
     printf("\n");
 }
